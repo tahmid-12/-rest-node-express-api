c42bc5a20220768389a64baa93bc90efcd64e1a7
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-15 21:06:06 +0600

    Express Post method completed.

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,18 @@
-const users = require('../models/users.models');
+let users = require('../models/users.models');
+const { v4: uuidv4 } = require("uuid");
 
 const getAllUsers = (req,res) => {
     res.status(200).json({ users });
 }
 
-module.exports = {getAllUsers};
+const createUser = (req,res) => {
+    const newUser = {
+        id: uuidv4(),
+        username: req.body.username,
+        email: req.body.email
+    }
+    users.push(newUser)
+    res.status(200).json(users);
+}
+
+module.exports = { getAllUsers,createUser };
