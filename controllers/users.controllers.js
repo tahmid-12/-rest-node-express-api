98ea94cb6dda0782e3e9284c05e992e543e80e82
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-15 21:41:47 +0600

    Express Update Method.

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,10 +1,12 @@
 let users = require('../models/users.models');
 const { v4: uuidv4 } = require("uuid");
 
+// Get all Users
 const getAllUsers = (req,res) => {
     res.status(200).json({ users });
 }
 
+//Create a User
 const createUser = (req,res) => {
     const newUser = {
         id: uuidv4(),
@@ -12,7 +14,19 @@ const createUser = (req,res) => {
         email: req.body.email
     }
     users.push(newUser)
+    res.status(201).json(users);
+}
+
+//Update a User
+const updateUser = (req, res) => {
+    const userId = req.params.id;
+    const { username, email } = req.body;
+    users.filter((user) => user.id === userId)
+         .map((selectedUser) => {
+            selectedUser.username = username;
+            selectedUser.email = email;
+         })
     res.status(200).json(users);
 }
 
-module.exports = { getAllUsers,createUser };
+module.exports = { getAllUsers,createUser,updateUser };
